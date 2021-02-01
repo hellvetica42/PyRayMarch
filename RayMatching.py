ae0902e275c564980f37a83312149111178d6e72
hellvetica42 <petar.slavicek@gmail.com>
2021-02-01 02:30:17 +0100

    Python is to slow for this. Single simple frame takes multiple seconds to compute at small resolutions. Should get back to cpp i guess

--- RayMatching.py
+++ RayMatching.py
@@ -3,10 +3,11 @@ import pygame #pylint: disable=import-error
 from Camera import Camera
 from Sphere import Sphere
 
+
 #res = (640, 480)
 res = (100, 100)
 
-maxSteps = 10
+maxSteps = 30
 
 c = Camera([5, 5], res, [0, 0, 0], 0, 5)
 
@@ -14,32 +15,45 @@ objects = [Sphere([20, 0, 0], 5, pygame.Color(200, 0, 0))]
 
 pygame.init()
 screen = pygame.display.set_mode(res)
+clock = pygame.time.Clock()
+rays, points = c.getRays()
 
 while True:
-    rays, points = c.getRays()
+
+    #For one object only
+    sphere = objects[0] 
+
+    tmpPoints = np.copy(points)
+    mask = np.full((res), False)
+    for i in range(maxSteps):
+
+        distances = sphere.getDistances(tmpPoints)
+        tmpRays = np.copy(rays)
+
+        tmpRays[:, :, 0] *= distances
+        tmpRays[:, :, 1] *= distances
+        tmpRays[:, :, 2] *= distances
+
+        tmpPoints = tmpPoints + tmpRays 
+
+        #Its that simple ?!?
+        newMask = distances <= 0
+        #Keeps track of the rays that hit the sphere
+        mask = np.logical_or(newMask, mask)
+
+
+    #mask[mask == True] = [200, 0, 0]
+    surfaceArray = np.zeros(points.shape)
+
     for x in range(res[0]):
         for y in range(res[1]):
-            point = np.copy(points[x][y])
+            if mask[x][y]:
+                surfaceArray[x][y] = [200, 0, 0]
 
-            i = 0
-            hit = False
-            while not hit:
-                i += 1
-
-                if i >= maxSteps:
-                    pygame.draw.circle(screen, pygame.Color(0, 0, 0), (x,y), 1)
-                    hit = True
-
-                step = min([o.getDistance(point) for o in objects])
-
-                if step <= 0:
-                    pygame.draw.circle(screen, pygame.Color(0, 0, 200), (x,y), 1)
-                    hit = True
-
-                point += rays[x][y] * step
-
-
+    surface = pygame.surfarray.make_surface(surfaceArray)
+    screen.blit(surface, (0,0))
 
 
     pygame.display.flip()
     print("frame")
+    clock.tick(60)
--- Sphere.py
+++ Sphere.py
@@ -6,7 +6,21 @@ class Sphere():
         self.radius = radius
         self.color = color
 
-    def getDistance(self, point):
-        point = np.array(point)
-        dis = np.linalg.norm(point - self.pos) - self.radius
-        return dis
+    def getDistances(self, points):
+        #Array of points of position of object
+        P = np.full((points.shape), self.pos)
+
+        #Euclidian distance
+        diff_vectors = P - points 
+        diff_vectors = diff_vectors**2
+
+        #Sum of each point's squared elements. now 2D array
+        sums = np.sum(diff_vectors, axis=2)
+
+        #Euclidian distance for each point
+        distances = np.sqrt(sums)
+        
+        #Spheres are simple
+        distances -= self.radius
+
+        return distances
